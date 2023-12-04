fa908a2709378f17e83eba6fe8668fce7c7ee87a
julianamary <143211963+julianamary@users.noreply.github.com>
2023-12-04 19:36:11 -0300

    4/12/2023

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -38,19 +38,24 @@ int main() {
     Clase *clases = new Clase[cantClases];
     eCodArchivos resClases = leerClases(clases, archivoClases);
     cout<< "Cantidad de clases: " << cantClases << endl;
-    uint cantAsistencias = -1;
+
+
+    //----------------------------------------------------------
+
+
+    uint cantAsistencias = -2;
     uint auxLeerAsistencias;
     while (!archivoAsistencias.eof()) {
         archivoAsistencias.read((char *)&auxLeerAsistencias, sizeof(uint));
         archivoAsistencias.read((char *)&auxLeerAsistencias, sizeof(uint));
-        Inscripcion auxInscripciones;
+        Inscripcion auxInscriptions;
         for (uint i = 0; i < auxLeerAsistencias; i++) {
-            archivoAsistencias.read((char *)&auxInscripciones, sizeof(Inscripcion));
+            archivoAsistencias.read((char *)&auxInscriptions, sizeof(Inscripcion));
         }
         cantAsistencias++;
     }
     cout << "Cantidad de asistencias: " << cantAsistencias << endl;
-    Asistencia *asistencias = new Asistencia[cantAsistencias - 1];
+    Asistencia *asistencias = new Asistencia[cantAsistencias];
     eCodArchivos resAsistencias = leerAsistencias(archivoAsistencias, asistencias, cantAsistencias);
     uint idReserva = numeroRandom(1, cantClases);
     uint idCliente = numeroRandom(1, cantClientes);
@@ -63,31 +68,18 @@ int main() {
         resizeAsistencia(sistema->asistencias, sistema->cantAsistencias);
 
     reservaFinal = ReservarClase(sistema, idReserva, idCliente);
-    ifstream ArchiFinal("../../asistencias_", ios::binary);
-    uint cantAsistenciasFinal = -1;
-    uint auxLeerAsistenciasFinal;
-    while (!ArchiFinal.eof()) {
-        ArchiFinal.read((char *)&auxLeerAsistenciasFinal, sizeof(uint));
-        ArchiFinal.read((char *)&auxLeerAsistenciasFinal, sizeof(uint));
-        Inscripcion auxInscripcionesFinal;
-        for (uint i = 0; i < auxLeerAsistenciasFinal; i++) {
-            ArchiFinal.read((char *)&auxInscripcionesFinal, sizeof(Inscripcion));
-        }
-        cantAsistenciasFinal++;
-    }
-    cout << "Cantidad de asistencias archivo nuevo: " << cantAsistenciasFinal << endl;
-    Asistencia *asistenciasFinal = new Asistencia[cantAsistenciasFinal - 1];
-    eCodArchivos resFinal = leerAsistencias(ArchiFinal, asistenciasFinal, cantAsistenciasFinal);
-    cout << resFinal;
+    cout << reservaFinal;
     delete[] clientes;
     delete[] clases;
+    Asistencia *auxLista = sistema->asistencias;
+    for(int i=0;i < sistema->cantAsistencias ; i++){
+            delete[] auxLista->ClasesInscriptas;
+            auxLista++;
+    }
     delete[] asistencias;
-    delete[] asistenciasFinal;
     archivoClientes.close();
     archivoClases.close();
     archivoAsistencias.close();
-    ArchiFinal.close();
-
     }
     delete sistema;
     return 0;
